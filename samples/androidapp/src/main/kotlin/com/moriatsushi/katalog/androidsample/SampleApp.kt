16a86e051ed353b16ff7e5560121a0019b535b84
Josip Parac <josip.parac@ouraring.com>
2026-04-21 16:23:54 +0200

    move previews to sample app

--- samples/androidapp/src/main/kotlin/com/moriatsushi/katalog/androidsample/SampleApp.kt
+++ samples/androidapp/src/main/kotlin/com/moriatsushi/katalog/androidsample/SampleApp.kt
@@ -1,7 +1,41 @@
 package com.moriatsushi.katalog.androidsample
 
 import android.app.Application
-import com.moriatsushi.katalog.androidsample.compose.material.composeMaterialGroup
+import com.moriatsushi.katalog.androidsample.compose.material.ButtonWithIcon
+import com.moriatsushi.katalog.androidsample.compose.material.SampleBottomAppBar
+import com.moriatsushi.katalog.androidsample.compose.material.SampleBottomNavigation
+import com.moriatsushi.katalog.androidsample.compose.material.SampleButton
+import com.moriatsushi.katalog.androidsample.compose.material.SampleCard
+import com.moriatsushi.katalog.androidsample.compose.material.SampleCheckbox
+import com.moriatsushi.katalog.androidsample.compose.material.SampleCircularProgressIndicator
+import com.moriatsushi.katalog.androidsample.compose.material.SampleDropdownMenu
+import com.moriatsushi.katalog.androidsample.compose.material.SampleExtendedFloatingActionButton
+import com.moriatsushi.katalog.androidsample.compose.material.SampleExtendedFloatingActionButtonFullWidth
+import com.moriatsushi.katalog.androidsample.compose.material.SampleFloatingActionButton
+import com.moriatsushi.katalog.androidsample.compose.material.SampleIcon
+import com.moriatsushi.katalog.androidsample.compose.material.SampleIconButton
+import com.moriatsushi.katalog.androidsample.compose.material.SampleIconToggleButton
+import com.moriatsushi.katalog.androidsample.compose.material.SampleLinearProgressIndicator
+import com.moriatsushi.katalog.androidsample.compose.material.SampleOutlinedButton
+import com.moriatsushi.katalog.androidsample.compose.material.SampleRadioButton
+import com.moriatsushi.katalog.androidsample.compose.material.SampleRadioButtonWithLabels
+import com.moriatsushi.katalog.androidsample.compose.material.SampleScaffold
+import com.moriatsushi.katalog.androidsample.compose.material.SampleSlider
+import com.moriatsushi.katalog.androidsample.compose.material.SampleSliderWithSteps
+import com.moriatsushi.katalog.androidsample.compose.material.SampleSnackbar
+import com.moriatsushi.katalog.androidsample.compose.material.SampleSnackbarWithSnackbarHost
+import com.moriatsushi.katalog.androidsample.compose.material.SampleSurface
+import com.moriatsushi.katalog.androidsample.compose.material.SampleSwitch
+import com.moriatsushi.katalog.androidsample.compose.material.SampleTabRow
+import com.moriatsushi.katalog.androidsample.compose.material.SampleText
+import com.moriatsushi.katalog.androidsample.compose.material.SampleTextButton
+import com.moriatsushi.katalog.androidsample.compose.material.SampleTextField
+import com.moriatsushi.katalog.androidsample.compose.material.SampleTextFieldForPassword
+import com.moriatsushi.katalog.androidsample.compose.material.SampleTextFieldIsError
+import com.moriatsushi.katalog.androidsample.compose.material.SampleTextFieldWithIcons
+import com.moriatsushi.katalog.androidsample.compose.material.SampleTextFieldWithMessage
+import com.moriatsushi.katalog.androidsample.compose.material.SampleTextFieldWithPlaceholder
+import com.moriatsushi.katalog.androidsample.compose.material.SampleTopAppBar
 import com.moriatsushi.katalog.ext.androidtheme.AndroidThemeExt
 import com.moriatsushi.katalog.ext.pagesaver.PageSaverExt
 import com.moriatsushi.katalog.ext.theme.ThemeExt
@@ -20,9 +54,123 @@ class SampleApp : Application() {
                 PageSaverExt(),
             ),
         ) {
-            group(
-                composeMaterialGroup,
-            )
+            compose("BottomAppBar") {
+                SampleBottomAppBar()
+            }
+            compose("BottomNavigation") {
+                SampleBottomNavigation()
+            }
+            group("Button") {
+                compose("Button") {
+                    SampleButton()
+                }
+                compose("Button with Icon") {
+                    ButtonWithIcon()
+                }
+            }
+            compose("Card") {
+                SampleCard()
+            }
+            compose("Checkbox") {
+                SampleCheckbox()
+            }
+            compose("CircularProgressIndicator") {
+                SampleCircularProgressIndicator()
+            }
+            compose("DropdownMenu") {
+                SampleDropdownMenu()
+            }
+            group("ExtendedFloatingActionButton") {
+                compose("ExtendedFloatingActionButton") {
+                    SampleExtendedFloatingActionButton()
+                }
+                compose("ExtendedFloatingActionButton full width") {
+                    SampleExtendedFloatingActionButtonFullWidth()
+                }
+            }
+            compose("FloatingActionButton") {
+                SampleFloatingActionButton()
+            }
+            compose("Icon") {
+                SampleIcon()
+            }
+            compose("IconButton") {
+                SampleIconButton()
+            }
+            compose("IconToggleButton") {
+                SampleIconToggleButton()
+            }
+            compose("LinearProgressIndicator") {
+                SampleLinearProgressIndicator()
+            }
+            compose("OutlinedButton") {
+                SampleOutlinedButton()
+            }
+            group("RadioButton") {
+                compose("RadioButton") {
+                    SampleRadioButton()
+                }
+                compose("RadioButton with labels") {
+                    SampleRadioButtonWithLabels()
+                }
+            }
+            compose("Scaffold") {
+                SampleScaffold()
+            }
+            group("Slider") {
+                compose("Slider") {
+                    SampleSlider()
+                }
+                compose("Slider with steps") {
+                    SampleSliderWithSteps()
+                }
+            }
+            group("Snackbar") {
+                compose("Snackbar") {
+                    SampleSnackbar()
+                }
+                compose("Custom with SnackbarHost") {
+                    SampleSnackbarWithSnackbarHost()
+                }
+            }
+            compose("Surface") {
+                SampleSurface()
+            }
+            compose("Switch") {
+                SampleSwitch()
+            }
+            compose("TabRow") {
+                SampleTabRow()
+            }
+            compose("Text") {
+                SampleText()
+            }
+            compose("TextButton") {
+                SampleTextButton()
+            }
+            group("TextField") {
+                compose("TextField") {
+                    SampleTextField()
+                }
+                compose("TextField with placeholder") {
+                    SampleTextFieldWithPlaceholder()
+                }
+                compose("TextField with icons") {
+                    SampleTextFieldWithIcons()
+                }
+                compose("TextField is error") {
+                    SampleTextFieldIsError()
+                }
+                compose("TextField with message") {
+                    SampleTextFieldWithMessage()
+                }
+                compose("TextField for password") {
+                    SampleTextFieldForPassword()
+                }
+            }
+            compose("TopAppBar") {
+                SampleTopAppBar()
+            }
         }
     }
 }
