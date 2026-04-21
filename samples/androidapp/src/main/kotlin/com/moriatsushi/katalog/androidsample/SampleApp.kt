4adfd47698f1b68cb8d459d02c5604d6c0c7e79d
Josip Parac <josip.parac@ouraring.com>
2026-04-21 15:01:21 +0200

    remove view based code

--- samples/androidapp/src/main/kotlin/com/moriatsushi/katalog/androidsample/SampleApp.kt
+++ samples/androidapp/src/main/kotlin/com/moriatsushi/katalog/androidsample/SampleApp.kt
@@ -1,14 +1,12 @@
 package com.moriatsushi.katalog.androidsample
 
 import android.app.Application
-import com.google.android.material.R as MaterialR
 import com.moriatsushi.katalog.androidsample.compose.material.composeMaterialGroup
-import com.moriatsushi.katalog.androidsample.fragment.fragmentGroup
-import com.moriatsushi.katalog.androidsample.view.material.viewMaterialGroup
 import com.moriatsushi.katalog.ext.androidtheme.AndroidThemeExt
 import com.moriatsushi.katalog.ext.pagesaver.PageSaverExt
 import com.moriatsushi.katalog.ext.theme.ThemeExt
 import com.moriatsushi.katalog.registerKatalog
+import com.google.android.material.R as MaterialR
 
 class SampleApp : Application() {
     override fun onCreate() {
@@ -24,8 +22,6 @@ class SampleApp : Application() {
         ) {
             group(
                 composeMaterialGroup,
-                viewMaterialGroup,
-                fragmentGroup,
             )
         }
     }
